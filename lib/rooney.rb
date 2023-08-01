ca8680eecac9e427ef1a80ca7a87b74bb5a13458
Lehi Sanchez <lsanchez@Lehis-Mac-Studio.local>
2023-08-01 13:19:37 -0400

    Initial commit

---
+++ lib/rooney.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rooney/version"
+
+module Rooney
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rooney_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Rooney do
+  it "has a version number" do
+    expect(Rooney::VERSION).not_to be nil
+  end
+end
