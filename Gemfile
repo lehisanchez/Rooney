435ee044061205925b7e65f3f4e976fd04673af6
Lehi Sanchez <17449+lehisanchez@users.noreply.github.com>
2023-08-01 15:12:01 -0400

    Updated Codecov test settings

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ gemspec
 gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 gem "rubocop", "~> 1.21"
+
+gem "simplecov", "~> 0.22.0"
+
+gem "simplecov-cobertura", "~> 2.1"
--- spec/rooney_spec.rb
+++ spec/rooney_spec.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
+
+require "simplecov-cobertura"
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 RSpec.describe Rooney do
   it "has a version number" do
     expect(Rooney::VERSION).not_to be nil
