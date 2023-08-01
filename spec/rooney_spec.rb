882d56eb3593bee6f56c3cba052156747ec78d48
Lehi Sanchez <17449+lehisanchez@users.noreply.github.com>
2023-08-01 15:35:00 -0400

    Updated Codecov test settings

--- spec/rooney_spec.rb
+++ spec/rooney_spec.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-require "simplecov"
-SimpleCov.start
-
-require "simplecov-cobertura"
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
-
 RSpec.describe Rooney do
   it "has a version number" do
     expect(Rooney::VERSION).not_to be nil
