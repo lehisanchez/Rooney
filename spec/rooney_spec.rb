acc072a419a381ca27cab827a84a9e92932fdf5b
Lehi Sanchez <17449+lehisanchez@users.noreply.github.com>
2023-08-02 06:04:01 -0400

    Added test to validate Rooney module

--- spec/rooney_spec.rb
+++ spec/rooney_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Rooney do
+  it "is valid" do
+    expect(Rooney).to be_a(Module)
+  end
+
   it "has a version number" do
     expect(Rooney::VERSION).not_to be nil
   end
