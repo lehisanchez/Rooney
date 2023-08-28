d68967ae6c6cccfb23a73376a585076b5dc5b8d6
Lehi Sanchez <17449+lehisanchez@users.noreply.github.com>
2023-08-28 09:24:13 -0400

    Updated RSpec test

--- spec/rooney/configuration_spec.rb
+++ spec/rooney/configuration_spec.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.describe Rooney::Configuration do
-  subject(:described_module) { Rooney }
-
+RSpec.describe Rooney do
   let(:api_key)    { ENV["API_KEY"] }
   let(:api_url)    { ENV["API_URL"] }
   let(:api_header) { ENV["API_HEADER"] }
 
   before do
-    described_module.configure do |config|
+    described_class.configure do |config|
       config.api_key    = api_key
       config.api_url    = api_url
       config.api_header = api_header
@@ -16,8 +14,8 @@ RSpec.describe Rooney::Configuration do
   end
 
   it "is expected to be configurable", :aggregate_failures do
-    expect(described_module.api_key).to eq api_key
-    expect(described_module.api_url).to eq api_url
-    expect(described_module.api_header).to eq api_header
+    expect(described_class.api_key).to eq api_key
+    expect(described_class.api_url).to eq api_url
+    expect(described_class.api_header).to eq api_header
   end
 end
